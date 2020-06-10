6aab009069f76bdbca0ef1d76759b5ec5c940866
James Soderstrom <jsoderst@uncc.edu>
2020-06-09 22:31:35 -0400

    Milestone 4 extra credit

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,6 @@
+document.getElementById("font-resize").addEventListener("click", resizeFont);
+document.getElementById("style-switch").addEventListener("click", changeBackground);
+
 function changeBackground(id = -1) {
     /*
     This function is used to change the background banner image and alt text on the blog homepage.
@@ -6,36 +9,38 @@ function changeBackground(id = -1) {
         1. When the general style switcher link at the top of the page is clicked, it will simply iterate through all possible backgrounds. The background will change each time the style switcher link is clicked.
         2. When one of the 3 buttons on the background is clicked, it will switch to a specific image. Each button is linked to a specific image. Repeatedly clicking the same button will not cause the image to continue to change.
     */
+
     var backgrounds = ["images/home-background.jpeg","https://images.pexels.com/photos/627654/pexels-photo-627654.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", "https://images.pexels.com/photos/1905045/pexels-photo-1905045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"];
     var alts = ["Picture of Road", "Picture of Mountains", "Picture of Water"];
 
-    // to handle the case when a specific button is clicked
-    switch(id) {
+    //to handle the case when a specific button is clicked
+    switch(id) 
+    {
         case 0:
-            document.getElementById("home-background").setAttribute("src", backgrounds[0]);
-            document.getElementById("home-background").setAttribute("alt", alts[0]);
+            document.getElementById("home-background").setAttribute("src", backgrounds[id]);
+            document.getElementById("home-background").setAttribute("alt", alts[id]);
             return;
         case 1:
-            document.getElementById("home-background").setAttribute("src", backgrounds[1]);
-            document.getElementById("home-background").setAttribute("alt", alts[1]);
+            document.getElementById("home-background").setAttribute("src", backgrounds[id]);
+            document.getElementById("home-background").setAttribute("alt", alts[id]);
             return;
         case 2:
-            document.getElementById("home-background").setAttribute("src", backgrounds[2]);
-            document.getElementById("home-background").setAttribute("alt", alts[2]);
+            document.getElementById("home-background").setAttribute("src", backgrounds[id]);
+            document.getElementById("home-background").setAttribute("alt", alts[id]);
             return;
     }
 
-    // retrieve current background and determine index of next background
+    //retrieve current background and determine index of next background
     var currentBackground = document.getElementById("home-background").getAttribute("src");
     var index = backgrounds.indexOf(currentBackground) + 1;
 
-    // return to beginning of backgrounds list if end has been reached
+    //return to beginning of backgrounds list if end has been reached
     if (index > 2) 
     {
         index = 0;
     }
 
-    // update background image and alt description
+    //update background image and alt description
     document.getElementById("home-background").setAttribute("src", backgrounds[index]);
     document.getElementById("home-background").setAttribute("alt", alts[index]);
 }
