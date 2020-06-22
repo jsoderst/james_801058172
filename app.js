99f078cf40b8b879689d58d452f4c12385376152
James Soderstrom <jsoderst@uncc.edu>
2020-06-22 17:59:18 -0400

    Restructuring project directory and creating app.js

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyparser = require('body-parser');
-const joi = require('@hapi/joi');
-const ejs = require('ejs');
+// const bodyparser = require('body-parser');
+// const joi = require('@hapi/joi');
+// const ejs = require('ejs');
 const fs = require('fs');
-const sqlite3 = require(sqlite3).verbose();
+// const sqlite3 = require(sqlite3).verbose();
 
+app.use('/public', express.static(path.join(__dirname, 'static')));
 
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.get('/images/home-background.jpeg', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'static/images', 'home-background.jpeg'));
+});
+
+// serving static css files
+fs.readdir('static/stylesheets', (err, files)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        for (let file of files){
+            app.get('/stylesheets/' + file, (req, res)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.sendFile(path.join(__dirname, 'static/stylesheets', file));
+                }
+            });
+        }
+    }
+});
+
+fs.readdir('static/scripts', (err, files)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        for (let file of files){
+            app.get('/scripts/' + file, (req, res)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.sendFile(path.join(__dirname, 'static/scripts', file));
+                }
+            });
+        }
+    }
+});
+
+app.listen(3000);
